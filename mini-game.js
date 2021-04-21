10f3f6d0d44efcf155909014e80af8d59bd76eac
Glitch (hello-webpage) <none>
2021-04-21 22:24:28 +0000

    🎏💐 Checkpoint ./ramen-toppings.js:30594676/236 ./mini-game.js:30594676/303 ./index.html:30594676/110

---
+++ mini-game.js
@@ -0,0 +1,1 @@
+//separating out the mini-game so it can be referenced later as a popup --- ramen-toppings.js will also need to be referenced for the objects to add on - we might not need to separate it out? We can always put it all here
---
+++ ramen-toppings.js
@@ -0,0 +1,18 @@
+//grouping of objects for the mini-ramen-game - will be referenced in mini-game
+
+function noodles(){
+  
+}
+
+function egg(){  
+  
+}
+
+function weed(){
+  
+}
+
+function broth(){
+  
+}
+
