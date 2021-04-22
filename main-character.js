c6f29daf55125132cb06bb5f166d4e8ae9330d67
Glitch (hello-webpage) <none>
2021-04-22 23:58:26 +0000

    🏢🚈 Checkpoint ./index.html:30594728/48 ./main-character.js:30594728/261

---
+++ main-character.js
@@ -0,0 +1,11 @@
+class MainCharacter {
+  constructor(){
+    this.pos = [10,10];
+    this.size = 10;
+    this.up = 38;
+    this.down = 40
+    this.right = 39;
+    this.left = 37;
+    this.select = 
+  }
+}
