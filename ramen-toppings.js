3ece252ab643ebbea7f170c6b9f9aa5979271a88
Glitch (hello-webpage) <none>
2021-04-22 01:48:35 +0000

    🌚🐛 Checkpoint ./ramen-toppings.js:30594728/274 ./index.html:30594728/196 ./style.css:30594728/253 ./sketch.js:30594728/101

--- ramen-toppings.js
+++ ramen-toppings.js
@@ -1,19 +1,30 @@
 //grouping of objects for the mini-ramen-game - will be referenced in mini-game
-const  ramen = {};
+// const  ramen = {};
 
-ramen.noodles = function (){
+// ramen.noodles = function (){
   
+// }
+
+// ramen.egg = function (){  
+  
+// }
+
+// ramen.weed = function (){
+  
+// }
+
+// ramen.broth = function (){
+  
+// }
+
+//some pseudo code to try and help me figure out some back end
+
+/*
+class Toppings(){
+  constructor(name,location[],image){
+    this.name = name
+    this.pos = location[]
+  }
 }
 
-ramen.egg = function (){  
-  
-}
-
-ramen.weed = function (){
-  
-}
-
-ramen.broth = function (){
-  
-}
-
+*/
