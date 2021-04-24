62f126b44e6dba862c42b38c03b6bcf7526f6f5a
Glitch (hello-webpage) <none>
2021-04-24 17:17:24 +0000

    ☄️🐜 Checkpoint ./index.html:30594728/91 ./canvas.js:30594728/5307

---
+++ canvas.js
@@ -0,0 +1,57 @@
+const noodles = new Topping("noodles",[100,100],"this should be an image");
+const character = new Character();
+
+const canvas = document.querySelector('#gameCanvas')
+
+
+
+const canvasSquare = 800;
+
+turtleCan.width = canvasSquare;
+turtleCan.height = canvasSquare;
+lineCan.width = canvasSquare;
+lineCan.height = canvasSquare;
+
+let lastTime = 0
+let aggregatedTime = 0
+const frameRateInMillis =  1000 / 20 // 30 FPS
+
+function setup(){
+  
+  window.requestAnimationFrame(loop);
+}
+
+function draw () {
+  lineCtx.fillStyle = "blue"
+  lineCtx.fillRect(0, 0, turtleCan.width, turtleCan.height);
+  // character.render();
+  // character.movement();
+  // noodles.render();
+  // noodles.select(character.hold);
+  // noodles.selected(character.pos);
+  // //console.log(character.grab)
+
+}
+
+function loop (time) { // Add the time parameter, and requestAnimation frame fills it in with the total time since starting
+	
+  let timeDifference = time - lastTime
+  //console.log(timeDifference) // Should be constant, probably around 16 (milliseconds)
+  lastTime = time
+	
+  // Add timeDifference to aggregatedTime
+  aggregatedTime += timeDifference
+  
+  if (aggregatedTime > frameRateInMillis) { // Divide by 1000 to get FPS
+  
+  	aggregatedTime = 0 // Reset aggregated time, it turtleCan also be carried over.
+  	
+    // DRAW HERE!
+    draw()
+  
+  }
+
+	window.requestAnimationFrame(loop)
+}
+
+setup();
