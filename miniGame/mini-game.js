c87d589c44f19181c5a12222b2501f3effb88e8a
Glitch (hello-webpage) <none>
2021-04-30 15:37:19 +0000

    🏰🐘 Checkpoint ./miniGame/mini-game.js:30594728/1556

--- miniGame/mini-game.js
+++ miniGame/mini-game.js
@@ -7,7 +7,7 @@ import { RamenBowl } from "./ramen-bowl.js"
 import {PlayerCharacter} from "../characters/player-character.js";
 import * as helperfunction from "./helper-functions.js"
 //declare global variables for this page
-var character, noodles, bowl;
+var character, noodles, chashu, bowl;
 
 //separating out the mini-game so it can be referenced later as a popup --- ramen-toppings.js will also need to be referenced for the objects to add on - we might not need to separate it out? We can always put it all here
 let ingredients = [];
@@ -18,6 +18,7 @@ miniGame.load = function() {
   character = new PlayerCharacter();
   bowl = new RamenBowl(canvas.width / 2, canvas.height / 2);
   noodles = [new Topping("noodles", 100, 100, "this should be an image")];
+  chashu = [new Topping("chashu", 300, 300, "this should be an image")]
 };
 
 miniGame.play = function() {
@@ -25,31 +26,18 @@ miniGame.play = function() {
 
   character.movement();
 
-  for (let i = 0; i < noodles.length; i++) {
-    if (
-      noodles[i].hasMoved &&
-      !noodles[i].isSelected &&
-      noodles[i + 1] === undefined
-    ) {
-      noodles.push(new Topping("noodles", 100, 100, "this should be an image"));
-    }
-    if (noodles[i].turnedIn) {
-      ingredients.push(noodles.splice(i, 1));
-    }
-
-    noodles[i].select(character.grab.flag, character.pos, bowl.pos);
-  }
+  toppingBuilder(noodles, "noodles", 100, 100, "this should be an image");
+  toppingBuilder(chashu, "chashu", 300, 300, "this should be an image");
+  
 
   
   // All drawing
   helperfunction.background();
   character.render();
   bowl.render();
-
-  for (let i = 0; i < noodles.length; i += 1) {
-    noodles[i].render();
-  }
-
+  toppingRender(noodles);
+  toppingRender(chashu);
+  
   //console.log(noodles.length)
   // noodles[0].render();
   //   if(!bowl.hasTopping(noodles, character.pos)){
@@ -60,3 +48,28 @@ miniGame.play = function() {
 };
 
 export { miniGame, character }
+
+function toppingBuilder(array, name, X, Y, image){
+  for (let i = 0; i < array.length; i++) {
+    if (
+      array[i].hasMoved &&
+      !array[i].isSelected &&
+      array[i + 1] === undefined
+    ) {
+      array.push(new Topping(name, X, Y, image));
+    }
+    if (array[i].turnedIn) {
+      ingredients.push(array.splice(i, 1));
+    }
+
+    array[i].select(character.grab.flag, character.pos, bowl.pos);
+  }
+  
+}
+
+function toppingRender(array){
+  for (let i = 0; i < array.length; i += 1) {
+    array[i].render();
+  }
+
+}
