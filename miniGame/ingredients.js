1d3bf1ee0f6b35441d7c81f50696e2c39fb1430e
Glitch (hello-webpage) <none>
2021-05-05 19:36:45 +0000

    🍿💧 Checkpoint ./miniGame/ingredients.js:30594728/426 ./style.css:30594676/10:30594728/382 ./index.html:30594728/9

--- miniGame/ingredients.js
+++ miniGame/ingredients.js
@@ -23,17 +23,23 @@ ingredients.generateBowl = function() {
     let j = Math.ceil((Math.random() * 10) % 6);
     switch(j){
       case 1:
-        ingredients.bowlIngredients.push(new Topping("noodles", 100, 100, "this should be an image", "#FAC600"));
+        ingredients.bowlIngredients.push(new Topping("noodles", 250+i*50, 10, "this should be an image", "#FAC600"));
         break;
       case 2:
-        ingredients.bowlIngredients.push(new Topping("chashu", 300, 100, "this should be an image", "#CE46E0"));
+        ingredients.bowlIngredients.push(new Topping("chashu", 250+i*50, 10, "this should be an image", "#CE46E0"));
         break;
       case 3:
-        ingredients.bowlIngredients.push(new Topping("kakuni", 100, 300, "this should be an image", "#59A8F7"));
+        ingredients.bowlIngredients.push(new Topping("kakuni", 250+i*50, 10, "this should be an image", "#59A8F7"));
         break;
       case 4:
-        ingredients.bowlIngredients.push(new Topping("nikuSoboro", 300, 300, "this should be an image", "#FF9652"));
-        
+        ingredients.bowlIngredients.push(new Topping("nikuSoboro", 250+i*50, 10, "this should be an image", "#FF9652"));
+        break;
+      case 5:
+        ingredients.bowlIngredients.push(new Topping("ajitama", 250+i*50, 10, "this should be an image", "#46E052"));
+        break;
+      case 6: 
+        ingredients.bowlIngredients.push(new Topping("menma", 250+i*50, 10, "this should be an image", "#4EFAEC"));
+        break;
     }
     
   }
@@ -41,7 +47,7 @@ ingredients.generateBowl = function() {
 
 ingredients.renderBowlIngredients = function () {
   for (let i = 0; i < ingredients.bowlIngredients.length; i+=1){
-    ingredients.bowlIngredients[i].pos = {x: 250+i*50, y: 10};
+    //ingredients.bowlIngredients[i].pos = {x: 250+i*50, y: 10};
     ingredients.bowlIngredients[i].render();
   }
   // if(ingredients.checkMatch()){
