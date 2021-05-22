1ca2557b5c5dc8ec4de199f6ceab2a82ad2e5413
Glitch (hello-webpage) <none>
2021-05-22 18:33:56 +0000

    🎾🚀 Checkpoint ./EndScreen.js:30594728/958 ./index.html:30594728/68

---
+++ EndScreen.js
@@ -0,0 +1,36 @@
+import { canvas, ctx, frameCount } from "../canvas.js";
+import * as helperFunction from "./helper-functions.js";
+
+const endScreen = {};
+
+endScreen.bg = document.getElementById("endScreen");
+endScreen.startGame = false;
+
+const buttons = { start: { top: 800, bottom: 900, left: 200, right: 500 } };
+
+endScreen.load = function() {
+  helperFunction.background(endScreen.bg);
+  ctx.fillStyle = "#f9cb9cff";
+  ctx.fillRect(
+    buttons.start.left,
+    buttons.start.top,
+    buttons.start.right - buttons.start.left,
+    buttons.start.bottom - buttons.start.top
+  );
+  // endScreen.click();
+};
+
+endScreen.click = function(event) {
+  if (
+    helperFunction.button(
+      buttons.start.top,
+      buttons.start.bottom,
+      buttons.start.left,
+      buttons.start.right
+    )
+  ) {
+    endScreen.startGame = true;
+  }
+};
+
+export { endScreen };
