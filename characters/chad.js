d9437f91955b97fcfd12ee06220662c9aaae3a33
Glitch (hello-webpage) <none>
2021-05-24 18:59:15 +0000

    🕵🏇 Checkpoint ./characters/chad.js:30594728/262

--- characters/chad.js
+++ characters/chad.js
@@ -2,9 +2,16 @@ import { canvas, ctx } from "../canvas.js";
 import * as helperFunction from "../helper-functions.js";
 
 const chad = {};
+chad.iamges = {listening: document.getElementById("chadL"), talking: document.getElementById("chadT")};
+
+chad.state = "idle"
 
 chad.render = function(){
-  ctx.drawImage
+  switch(chad.state){
+    case "listening":
+      ctx.drawImage()
+  }
+  
 }
 
 
