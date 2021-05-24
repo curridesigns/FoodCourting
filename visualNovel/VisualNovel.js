15110228d3610f44d36b5041801eb63639beb377
Glitch (hello-webpage) <none>
2021-05-24 15:54:26 +0000

    🛢🌻 Checkpoint ./visualNovel/rooms/arcade-counter.js:30594728/270 ./visualNovel/VisualNovel.js:30594728/362

--- visualNovel/VisualNovel.js
+++ visualNovel/VisualNovel.js
@@ -1,18 +1,23 @@
 import { canvas, ctx, frameCount } from "../canvas.js";
 import * as helperFunction from "../helper-functions.js";
+import { arcade } from "./rooms/arcade-counter.js"
 
 const visualNovel = {};
 
 visualNovel.bg = document.getElementById("mallMap");
 
 visualNovel.display = false
+visualNovel.arcadeDisplay = false
 
 const buttons = { store: { top: 275, bottom: 575, left: 77, right:  710}, spicy: { top: 275, bottom: 470, left: 1095, right:  1415}, arcade: { top: 275, bottom: 575, left: 1430, right:  1830}, food: { top: 590, bottom: 870, left: 725, right:  1070} };
 
 visualNovel.load = function() {
   
   helperFunction.background(visualNovel.bg);
-  // endScreen.click();
+
+  if(visualNovel.arcadeDisplay){
+    arcade.load();
+  }
 };
 
 visualNovel.click = function(event) {
@@ -45,7 +50,8 @@ visualNovel.click = function(event) {
       buttons.arcade.right
     )
   ) {
-    console.log("arcade");
+    visualNovel.arcadeDisplay = true;
+    visualNovel.display = false;
   }
   if (
     helperFunction.button(
--- visualNovel/rooms/arcade-counter.js
+++ visualNovel/rooms/arcade-counter.js
@@ -1,16 +1,21 @@
-import { canvas, ctx, frameCount } from "../canvas.js";
-import * as helperFunction from "../helper-functions.js";
+import { canvas, ctx, frameCount } from "../../canvas.js";
+import * as helperFunction from "../../helper-functions.js";
 
-const visualNovel = {};
+const arcade = {};
+arcade.bg = document.getElementById("arcadeCounterBG");
+arcade.Reg = document.getElementById("arcadeCounterReg")
 
-visualNovel.bg = document.getElementById("mallMap");
-
-visualNovel.display = false
+arcade.display = false
 
 // const buttons = { store: { top: 275, bottom: 575, left: 77, right:  710}, spicy: { top: 275, bottom: 470, left: 1095, right:  1415}, arcade: { top: 275, bottom: 575, left: 1430, right:  1830}, food: { top: 590, bottom: 870, left: 725, right:  1070} };
 
-visualNovel.load = function() {
+arcade.load = function() {
   
-  helperFunction.background(visualNovel.bg);
+  helperFunction.background(arcade.bg);
+  
+  helperFunction.background(arcade.Reg);
   // endScreen.click();
 };
+
+
+export { arcade };
