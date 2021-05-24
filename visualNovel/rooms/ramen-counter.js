5afa6f6990a5b2b8098f78aa472e16f4d9a1aea2
Glitch (hello-webpage) <none>
2021-05-24 18:47:53 +0000

    🍩🍯 Checkpoint ./visualNovel/rooms/kitchen.js:30594559/830 ./visualNovel/rooms/ramen-counter.js:30594559/784 ./visualNovel/rooms/spencers-counter.js:30594559/808

---
+++ visualNovel/rooms/ramen-counter.js
@@ -0,0 +1,25 @@
+import { canvas, ctx, frameCount } from "../../canvas.js";
+import * as helperFunction from "../../helper-functions.js";
+import { visualNovel } from "../VisualNovel.js"
+
+const ramen = {};
+//ramen.bg = document.getElementById("");
+//ramen.Reg = document.getElementById("")
+
+const buttons = { returnToMap: { top: 1010, bottom: 1110, left: 1650, right: 1850 }};
+
+ramen.load = function() {
+  
+  //helperFunction.background(ramen.bg);
+  //helperFunction.background();
+  //helperFunction.background(ramen.Reg);
+  //dialogue
+  visualNovel.returnToMapRender(buttons, "#f9cb9c55");
+  // endScreen.click();
+};
+
+ramen.click = function(event) {
+  visualNovel.returnToMapClick(event, buttons);
+}
+
+export { ramen };
---
+++ visualNovel/rooms/spencers-counter.js
@@ -0,0 +1,25 @@
+import { canvas, ctx, frameCount } from "../../canvas.js";
+import * as helperFunction from "../../helper-functions.js";
+import { visualNovel } from "../VisualNovel.js"
+
+const spencers = {};
+//spencers.bg = document.getElementById("");
+//spencers.Reg = document.getElementById("")
+
+const buttons = { returnToMap: { top: 1010, bottom: 1110, left: 1650, right: 1850 }};
+
+spencers.load = function() {
+  
+  //helperFunction.background(spencers.bg);
+  //helperFunction.background();
+  //helperFunction.background(spencers.Reg);
+  //dialogue
+  visualNovel.returnToMapRender(buttons, "#f9cb9c55");
+  // endScreen.click();
+};
+
+spencers.click = function(event) {
+  visualNovel.returnToMapClick(event, buttons);
+}
+
+export { spencers };
