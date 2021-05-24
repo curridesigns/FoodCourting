0473b9ad29934eb709149e58300e768dbe37b2b7
Glitch (hello-webpage) <none>
2021-05-24 15:20:19 +0000

    👳⏰ Checkpoint ./visualNovel/VisualNovel.js:30594728/909

--- visualNovel/VisualNovel.js
+++ visualNovel/VisualNovel.js
@@ -7,7 +7,7 @@ visualNovel.bg = document.getElementById("mallMap");
 
 visualNovel.display = false
 
-const buttons = { store: { top: 350, bottom: 575, left: 77, right:  710} };
+const buttons = { store: { top: 275, bottom: 575, left: 77, right:  710}, spicy: { top: 275, bottom: 470, left: 1095, right:  1415}, arcade: { top: 275, bottom: 575, left: 1430, right:  1830}, food: { top: 590, bottom: 870, left: 725, right:  1070} };
 
 visualNovel.load = function() {
   
@@ -17,6 +17,36 @@ visualNovel.load = function() {
 
 visualNovel.click = function(event) {
   console.log(helperFunction.getMouseCoordinates(event));
+  if (
+    helperFunction.button(
+      buttons.store.top,
+      buttons.store.bottom,
+      buttons.store.left,
+      buttons.store.right
+    )
+  ) {
+    console.log("store");
+  }
+  if (
+    helperFunction.button(
+      buttons.spicy.top,
+      buttons.spicy.bottom,
+      buttons.spicy.left,
+      buttons.spicy.right
+    )
+  ) {
+    console.log("spicy");
+  }
+  if (
+    helperFunction.button(
+      buttons.store.top,
+      buttons.store.bottom,
+      buttons.store.left,
+      buttons.store.right
+    )
+  ) {
+    console.log("store");
+  }
   if (
     helperFunction.button(
       buttons.store.top,
