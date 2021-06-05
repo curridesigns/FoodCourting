f744795cc9f17ac2d261685657aa3c3b82c5172d
Glitch (hello-webpage) <none>
2021-06-05 04:27:40 +0000

    🏆🏜 Checkpoint ./characters/Raven.js:30594676/4745

--- characters/Raven.js
+++ characters/Raven.js
@@ -83,6 +83,230 @@ text[1] = {
   ]
 };
 
+text[2] = {
+  response: [
+    {
+      a: {
+        text: "Ugh, you just missed the most annoying kids.",
+        playerChoices: 1,
+        talkingTime: 1
+      }
+    },
+    {
+      a: {
+        text: "Hot pink hair, band shirts, bad eyeliner, the works.",
+        playerChoices: 0,
+        talkingTime: 1 },
+      b: {
+        text: "If only you were here earlier so we could make fun of them together.",
+        playerChoices: 0,
+        talkingTime: 1
+      },
+      c: {
+        text: "Haha right? To only have to worry about your eyeliner and what Syndey said online.",
+        playerChoices: 0,
+        talkingTime: 1
+      }
+    }
+  ],
+  choice: [
+    { complete: "Return to Map" },
+    { a: "Oh thank god", b: "Aw shucks", c: "Ah to be a cringey kid" }
+  ]
+};
+
+text[3] = {
+  response: [
+    {
+      a: {
+        text: "There's a local concert this weekend, I'm waiting for my manager to let me know if I have Saturday off or not.",
+        playerChoices: 1,
+        talkingTime: 1
+      }
+    },
+    {
+      a: {
+        text: "Dead Mouse Trap",
+        playerChoices: 0,
+        talkingTime: 1 },
+      b: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      },
+      c: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      }
+    }
+  ],
+  choice: [
+    { complete: "Return to Map" },
+    { a: "What's the band?", b: "Wannabe grungey, I like it.", c: "All managers suck at that, huh." }
+  ]
+};
+
+text[4] = {
+  response: [
+    {
+      a: {
+        text: "",
+        playerChoices: 1,
+        talkingTime: 1
+      }
+    },
+    {
+      a: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1 },
+      b: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      },
+      c: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      }
+    }
+  ],
+  choice: [
+    { complete: "Return to Map" },
+    { a: "", b: "", c: "" }
+  ]
+};
+
+text[5] = {
+  response: [
+    {
+      a: {
+        text: "",
+        playerChoices: 1,
+        talkingTime: 1
+      }
+    },
+    {
+      a: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1 },
+      b: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      },
+      c: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      }
+    }
+  ],
+  choice: [
+    { complete: "Return to Map" },
+    { a: "", b: "", c: "" }
+  ]
+};
+
+text[6] = {
+  response: [
+    {
+      a: {
+        text: "",
+        playerChoices: 1,
+        talkingTime: 1
+      }
+    },
+    {
+      a: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1 },
+      b: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      },
+      c: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      }
+    }
+  ],
+  choice: [
+    { complete: "Return to Map" },
+    { a: "", b: "", c: "" }
+  ]
+};
+
+text[7] = {
+  response: [
+    {
+      a: {
+        text: "",
+        playerChoices: 1,
+        talkingTime: 1
+      }
+    },
+    {
+      a: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1 },
+      b: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      },
+      c: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      }
+    }
+  ],
+  choice: [
+    { complete: "Return to Map" },
+    { a: "", b: "", c: "" }
+  ]
+};
+
+text[8] = {
+  response: [
+    {
+      a: {
+        text: "",
+        playerChoices: 1,
+        talkingTime: 1
+      }
+    },
+    {
+      a: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1 },
+      b: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      },
+      c: {
+        text: "",
+        playerChoices: 0,
+        talkingTime: 1
+      }
+    }
+  ],
+  choice: [
+    { complete: "Return to Map" },
+    { a: "", b: "", c: "" }
+  ]
+};
+
 /*const boxes = {
   main: { top: 700, bottom: 850, left: 100, right: 1000 },
   choices: {
